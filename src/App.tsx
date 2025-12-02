4c85e2602ddbd6659d0861e7e2b6d0ae40c687a7
eflores <dani19floress@gmail.com>
2025-12-01 23:08:57 -0600

    feat: Implement application routing with main layout and new pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { DashboardPage } from './presentation/pages/DashboardPage';
 import { ReportsPage } from './presentation/pages/ReportsPage';
