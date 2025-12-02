0c43a69dd68d4a4ca39ce26e11093c5ae2ddfe41
eflores <dani19floress@gmail.com>
2025-12-01 23:12:01 -0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { DashboardPage } from './presentation/pages/DashboardPage';
 import { ReportsPage } from './presentation/pages/ReportsPage';
